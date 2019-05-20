c082264eb4f583edcf700a6546bbd12094beb20f
Christopher Specht <cfspecht@users.noreply.github.com>
2019-05-20 15:44:17 -0700

    start on cnt_freq

--- huffman.py
+++ huffman.py
@@ -14,4 +14,25 @@ class Node:
         self.left = left
         self.right = right
 
-    #to-do: write the repr and eq methods
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+
+def cnt_freq(filename):
+    """ Opens a text file and counts frequency of occurrences of all characters
+    Index of a list is the ord() of a character
+    Args:
+        filename (str): file to be opened
+    Returns:
+        list: list of frequencies of characters
+    """
+    # initialize empty list of size 256
+    freqlist = [0] * 256
+
+    # read target file
+    fp = open(filename, "r")
+    lines = fp.readlines()
+    fp.close()
